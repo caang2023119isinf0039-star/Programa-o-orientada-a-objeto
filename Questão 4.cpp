e8be3812688f91d1d80b9c8c425c83d21244cd8b
caang2023119isinf0039-star <caang.2023119isinf0039@aluno.ifpi.edu.br>
2025-12-03 15:45:07 -0300

    Create Questão 4.cpp

---
+++ Questão 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    //  Resposta da quarta 
+    int num[5];
+    for(int i = 0; i< 5;i++)
+    {
+        std::cout << "Digite um número: ";
+        std::cin >> num[i];
+        soma =+ num[i];
+    }
+    std::cout << "Os valores digitados foram: \n";
+    for(int i = 0; i<5; i++)
+    {
+        std::cout << num[i] << "\n";
+    }
